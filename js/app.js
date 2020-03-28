7461e44bd48832bf8f23dc53c686d5351cfd07d0
moumen-soliman <moumensoliman@gmail.com>
2020-03-28 16:00:39 +0200

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,35 @@
+// Global Variables 
+
+const pageSections = document.querySelectorAll("section");
+const navbar = document.getElementById("navbar__list");
+
+// Build Nav
+
+for (let i = 0; i < pageSections.length; i++) {
+    const newLi = document.createElement("li");
+    const sections = document.getElementsByTagName("section");
+    newLi.innerHTML = `<a href="#${sections[i].id}">${sections[i].id}</a>`;
+    navbar.appendChild(newLi);
+};
+
+// Add class 'active' to section when near top of viewport
+
+function inViewport(element) {
+    let bounding = element.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+window.addEventListener("scroll", function() {
+    for (const section of pageSections) {
+        if (inViewport(section)) {
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    }
+});
